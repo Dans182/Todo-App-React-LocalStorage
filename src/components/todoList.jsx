a2a08c844cae667f447421ca60461a5d8d6e3b81
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-04-19 17:58:57 +0200

    v1.4

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
 
 const TodoList = () => {
-    
-    return(
-        <h1>Soy TodoList</h1>
-    )
-}
+  return (
+    <div>
+      <h1 className="text-right">Soy TodoList</h1>
+
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title text-end">
+              Titulo de la tarea
+              
+            </h3>
+            <button className="btn btn-sm btn-success">Terminar</button>
+          <p className="card-text text-end">Descripción de la tarea</p>
+          <hr />
+          <div className="d-flex justify-content-end">
+            <button className="btn btn-sm btn-primary me-2">Editar</button>
+            <button className="btn btn-sm btn-danger">Eliminar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
