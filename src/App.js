836ca9adf7a623c8afa76ee2e9a1edca92f6f3a6
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-04-19 17:26:22 +0200

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-    return (
-        // <> -->fragment, es un contenedor padre
-        <>
-            <h1>Soy el App</h1>
-            <p>Soy un parrafo</p>
-        </>
-    );
-}
+  return (
+    // <> -->fragment, es un contenedor padre
+    <div className="containet ,t-4">
+      <div className="row">
+        <div className="col-8">
+          <h1>Lista de tareas</h1>
+        </div>
+        <div className="col-4">
+          <h1>Formulario</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
